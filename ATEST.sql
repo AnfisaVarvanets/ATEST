e23ad8d2337123aa621aa69e078b3d0c8f320d80
Anfisa <51121339+AnfisaVarvanets@users.noreply.github.com>
2019-06-24 13:55:28 +0300

    Update ATEST.sql

--- ATEST.sql
+++ ATEST.sql
@@ -9,10 +9,3 @@ go
 
 
 
-select Distinct ProductName,CategoryID,RetailPrice
-from  Products inner join Product_Vendors
-on Products.ProductNumber = Product_Vendors.ProductNumber
-inner join Vendors
-on Product_Vendors.VendorID =  Vendors.VendorID
-Order by ProductName ASC
-go
