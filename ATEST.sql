c2e0f5871bb9cc42ee9b615a85306dcec4f17995
Anfisa <51121339+AnfisaVarvanets@users.noreply.github.com>
2019-06-24 13:54:57 +0300

    Update ATEST.sql

--- ATEST.sql
+++ ATEST.sql
@@ -5,3 +5,14 @@ inner join Vendors
 on Product_Vendors.VendorID =  Vendors.VendorID
 Order by ProductName ASC
 go
+
+
+
+
+select Distinct ProductName,CategoryID,RetailPrice
+from  Products inner join Product_Vendors
+on Products.ProductNumber = Product_Vendors.ProductNumber
+inner join Vendors
+on Product_Vendors.VendorID =  Vendors.VendorID
+Order by ProductName ASC
+go
